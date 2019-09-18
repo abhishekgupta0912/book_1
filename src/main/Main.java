e5086262bd85276fd081d17962cad66b023171c7
abhishek <abhishek.gupta1_cs18@gla.ac.in>
2019-09-18 23:00:59 +0530

    program created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,53 @@
 package main;
 
+import java.util.Scanner;
+
 class Book {
-    private String bname, author, isbbn;
+    private String bname, author, isbn;
+
+    Book(String nam, String auth, String ISBN) {
+        this.bname = nam;
+        this.author = auth;
+        this.isbn = ISBN;
+    }
 
     public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String na) {
+        this.bname = na;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 }
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 0)
+            System.out.println("N/a");
+        else {
+            Book[] ob = new Book[n];
+            for (int i = 0; i < n; i++) {
+                ob[i] = new Book(sc.nextLine(), sc.nextLine(), sc.nextLine());
+            }
+        }
+    }
 
 }
